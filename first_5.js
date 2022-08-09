60924f9a34652e03fe54e0241ebd12dd5acf11a3
tahdi-islam <tshdi60@gmail.com>
2022-08-10 03:07:43 +0600

    solve frist 4 problem

---
+++ first_5.js
@@ -0,0 +1,57 @@
+// 1st problem
+
+const taka = 1000;
+const orange = 300;
+const apple = 320;
+
+const totalPrice = apple + orange;
+
+const shopkeersReturn = taka - totalPrice;
+
+// console.log(shopkeersReturn)
+
+// 2nd problem
+
+const math = 90;
+const physics = 87;
+const english = 70;
+const bangla = 66;
+const chemistry = 87;
+const biology = 85;
+
+const totalScore = math + physics + english + chemistry + biology;
+const averageScore = totalScore / 6;
+
+// console.log(averageScore)
+
+
+// 3rd problem 
+
+const text1 = "I am going to be an ";
+
+const text2 = "awesome developer";
+
+const finalText = text1 + text2;
+
+// console.log(finalText)
+
+// 4th problem 
+
+const number1 = 119;
+
+const dividerNumer = 5;
+
+const reminderNumber = 119 % 5;
+
+// console.log(reminderNumber)
+
+// 5th problem 
+/* 
+    Var price = 33 --> has no semicolon
+    var name - Shabana --> has no equal sign (=)
+    var boxName = ‘Cocola; --> has no endign quote
+    var 88_price = 34; --> variable name is start with number
+    var enum = -1; --> variable name is a keyword 
+    var _$box’78 = ‘Monika’; --> variable name has a quote
+    var home-address = “kochu khet”; --> variable name has a (-) sign 
+*/
