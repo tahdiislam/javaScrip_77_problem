0fa7bde747d30a1641dfbb041f08c390254cd2b1
tahdi-islam <tshdi60@gmail.com>
2022-08-10 06:30:50 +0600

    solve prb 16 to 20

---
+++ prb16To20.js
@@ -0,0 +1,42 @@
+// 16th problem
+
+function myMoney(money){
+    let text
+    if(money >= 80000){
+        text = "I can buy a Mac Book"
+    } else if(money >= 60000){
+        text = "I can buy a gaming laptop"
+    } else if(money >= 40000){
+        text = "I can buy a lenevo yuga"
+    } else if(money >= 20000){
+        text = "I can buy a second hand laptop"
+    } else{
+        text = "I am happy with my phone"
+    }
+    return text;
+}
+
+const money = 65000;
+
+const iCanAffort = myMoney(money);
+// console.log(iCanAffort)
+
+// 17th problem
+
+let text = "আসকে আমার মন ভালো নেই";
+for(i = 0; i <= 39; i++){
+    // console.log(text)
+}
+
+// 19th problem
+
+for(i = 58; i <= 98; i++){
+    // console.log(i)
+}
+
+// 20th problem
+
+for(i = 412; i <= 456; i += 2){
+    // console.log(i)
+}
+
